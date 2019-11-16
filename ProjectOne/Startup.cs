e20bc5058d20aea8e5f36aec9caf2081ce1f5e44
jacenelson2018 <jacenelson15@gmail.com>
2019-11-16 11:31:47 -0700

    first commit

---
+++ ProjectOne/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ProjectOne.Startup))]
+namespace ProjectOne
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
